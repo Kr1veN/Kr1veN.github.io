1755e5cbc0bea25a0d6c88ee012f4ec9cfd9ee1b
Daniel Borrego Cruz <danibc2400@gmail.com>
2026-09-19 15:41:48 +0200

    s

--- assets/js/viewer.js
+++ assets/js/viewer.js
@@ -228,7 +228,7 @@ function createViewerController(root) {
       const action = btn.dataset.mode;
       if (action === "compare") {
         const names = Object.keys(layerByName);
-        setCompareMode(names[1], names[names.length - 1]);
+        setCompareMode(names[0], names[names.length]);
       } else if (action === "blend") {
         setBlendMode(Object.keys(layerByName));
       } else {
